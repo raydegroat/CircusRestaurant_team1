8aa5e14366a7ea15b4f5ea7780c9758a467e1408
Ray de Groat <61622150+raydegroat@users.noreply.github.com>
2020-06-26 13:01:06 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -7,12 +7,18 @@
         let wheelCounter = '<input type="number" id="quantity" name="quantity" min="1" max="10" style="width: 30px" value="1" >';
                         
 
-            jQuery('.name').click(function(){
-                $("#order").append(wheelCounter+' '+ $(this).text()+'<br>');
-               console.log($(this).text());
+            jQuery('.name').click(function(){    /* When name is clicked, it will be added to the orderUp list*/
+                $("#orderUp").append('<li>'+wheelCounter+ $(this).text()+'</li>');
+            //    console.log($(this)); 
             });
 
-            jQuery("#placeorder").click(function(){
+            jQuery("#placeorder").click(function(){  /* Hide the order button and notify user that the order was received */
+                $(this).hide();
                 alert("Thank you for your order!");
+                $("#orderUp").empty();
+                $("#orderInstructions").replaceWith("<h4>Thank you! Your order is on it's way!</h4>")
             })
         });
+
+
+
