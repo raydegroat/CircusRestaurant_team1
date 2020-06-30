6bcd268ddbdb4575acc9ca41c2496cbc2127dded
Ray de Groat <61622150+raydegroat@users.noreply.github.com>
2020-06-30 11:38:26 +0200

    Delete book-table-circus.js

--- circus-rest-moble/js/book-table-circus.js
+++
@@ -1,9 +0,0 @@
-function postMessage(msg, myYes){
-   let confirmBox = $("#confirm");
-            confirmBox.find(".message").text(msg);
-            confirmBox.find(".yes").unbind().click(function() {
-               confirmBox.hide();
-            });
-            confirmBox.find(".yes").click(myYes);
-            confirmBox.show();
-}
